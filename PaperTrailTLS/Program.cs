e27321d18b92c1f9ef6c82916b3e9b297ef98fee
MatthewFitchett <fitchett.matthew@googlemail.com>
2014-03-28 09:09:50 +0000

    Initial commit

---
+++ PaperTrailTLS/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace PaperTrailTLS
+{
+    class Program
+    {
+        public static int Main(string[] args)
+        {
+            string serverCertificateName = null;
+            string machineName = "logs.papertrailapp.com";
+            int port = 24919;
+
+            // serverCertificateName name must match the name on the server's certificate. 
+            serverCertificateName = machineName;
+
+            SslTcpClient.RunClient(machineName,port, serverCertificateName);
+            return 0;
+        }
+    }
+}
